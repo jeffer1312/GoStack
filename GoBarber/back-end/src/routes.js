c109667444455334bb91ce9753f78c16ca72a508
Jefferson Felizardo <jeffer1312@gmail.com>
2020-02-05 11:38:43 -0300

    implementando multer para upload de fotos

--- GoBarber/back-end/src/routes.js
+++ GoBarber/back-end/src/routes.js
@@ -1,13 +1,19 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import userController from './app/controllers/UserController';
 import sessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', userController.store);
 routes.post('/sessions', sessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', userController.update);
+routes.post('/files', upload.single('file'), (req, res) => {
+  res.json({ ok: true });
+});
 
 export default routes;
