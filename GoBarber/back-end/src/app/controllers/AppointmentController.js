1409a4e83ae51683c64936a0c74957e0ba518e30
Jefferson Felizardo <jeffer1312@gmail.com>
2020-02-05 14:28:05 -0300

    implementando agendamentos

---
+++ GoBarber/back-end/src/app/controllers/AppointmentController.js
@@ -0,0 +1,9 @@
+import Appointment from '../models/Appointments';
+
+class AppointmentController {
+  async store(req, res) {
+    res.json({});
+  }
+}
+
+export default new AppointmentController();
--- GoBarber/back-end/src/routes.js
+++ GoBarber/back-end/src/routes.js
@@ -2,7 +2,10 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 import userController from './app/controllers/UserController';
+import fileController from './app/controllers/FileController';
+import providerController from './app/controllers/ProviderController';
 import sessionController from './app/controllers/SessionController';
+import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -12,8 +15,8 @@ routes.post('/users', userController.store);
 routes.post('/sessions', sessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', userController.update);
-routes.post('/files', upload.single('file'), (req, res) => {
-  res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), fileController.store);
+routes.get('/providers', providerController.index);
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
